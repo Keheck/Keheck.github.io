8b51c48d7465b706270dba71cfe5513c533dd41d
Keheck <keheck@web.de>
2019-07-23 23:48:44 +0200

    Update pwskript.php

--- pwskript.php
+++ pwskript.php
@@ -1,29 +1,3 @@
 <?php
-$pw = "";
-if(isset($_GET['q']))
-{
-	$pw = $_GET['q'];
-}
-$pwscore = strlen($pw);
-if(preg_match("/[a-z/", $pw))
-{
-	$pwscore += 5;
-}
-if(preg_match("/[A-Z]/", $pw))
-{
-	$pwscore += 5;
-}
-if(preg_match("/[0-9]/", $pw))
-{
-	$pwscore += 5;
-}
-if(preg_match("/[,.-;:_]/", $passwort))
-{
-	$pwscore += 5;
-}
-
-if($pwscore <= 18)
-{
-	echo 'unsicher';
-}
+echo "test!!";
 ?>
